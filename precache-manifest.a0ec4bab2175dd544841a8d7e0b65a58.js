ae80373dd2b585f268e1e62db0c6be28a6b52338
Mohit Khanwale <mohit.khanwale1@gmail.com>
2019-10-07 20:29:11 +0530

    Updates

---
+++ precache-manifest.a0ec4bab2175dd544841a8d7e0b65a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf2d5c2b6dfc553d36ded5678d23d1b",
+    "url": "/hey/index.html"
+  },
+  {
+    "revision": "7dacc2217e841983eada",
+    "url": "/hey/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "129ddc5ea8ae815df87b",
+    "url": "/hey/static/js/2.601f7fe6.chunk.js"
+  },
+  {
+    "revision": "7dacc2217e841983eada",
+    "url": "/hey/static/js/main.22c65402.chunk.js"
+  },
+  {
+    "revision": "c88a330109dba01f62eb",
+    "url": "/hey/static/js/runtime-main.30146a20.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hey/static/media/logo.25bf045c.svg"
+  }
+]);
